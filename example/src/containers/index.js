23da8fe357e191b6b90f0de5eefead7cdceeedd9
Nicolas <zhaoxd@zenmen.com>
2019-01-29 14:34:52 +0800

    feat: rename styles folder

--- example/src/containers/index.js
+++ example/src/containers/index.js
@@ -1,6 +1,6 @@
 import './polyfills';
 import 'config/public-path';
-import 'styles/base.scss';
+import 'assets/base.scss';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
