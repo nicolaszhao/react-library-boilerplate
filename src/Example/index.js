8917088f389e0ad46bea33a3377eb637e66e0ccc
Nicolas <zhaoxd@zenmen.com>
2019-11-20 21:06:13 +0800

    feat: 调整示例代码

--- src/Example/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.scss';
-
-const Example = () => (
-  <div className="example">Example!</div>
-);
-
-export default Example;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export { default as Example } from './Example';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './index.scss';
+
+const ExampleComponent = ({ text }) => (
+  <div className="example-component">Example Component: {text}</div>
+);
+
+export default ExampleComponent;
